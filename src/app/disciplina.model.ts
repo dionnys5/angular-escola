e69b9e041b919f58f07f7f8470ab5cdbaf7daf7c
Dionnys Santos Marinho <dionnys.santos@ulbra.edu.br>
2018-02-27 13:48:42 -0300

    Trabalho programação web

--- src/app/disciplina.model.ts
+++ src/app/disciplina.model.ts
@@ -1,8 +1,14 @@
+import { Professor } from "./professor.model";
+
 export class Disciplina {
+  cod: number;
   nome: string;
   descricao: string;
+  professor: Professor;
 
-  constructor(nome: string, descricao?: string) {
+  constructor(cod: number, nome: string, professor:Professor, descricao?: string) {
+    this.cod = cod;
+    this.professor = professor;
     this.nome = nome;
     this.descricao = descricao;
   }
