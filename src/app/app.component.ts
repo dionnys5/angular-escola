4807f267eeaba6a7e855d5120261adfafc362d14
Dionnys Santos Marinho <dionnys.santos@ulbra.edu.br>
2018-03-26 07:41:37 -0300

    LocalStorage inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,62 +13,11 @@ export class AppComponent {
   nome = null;
   tipo = null;
   data = null;
-  status = null;
+  status = false;
   periodos = [1,2,3,4,5,6,7,8]
   periodo = null;
   descricao = null;
-  disciplinas = [
-    new Disciplina(1, 'Língua Portuguesa','primaria','2017-01-01',true,1, 'O objetivo norteador da BNCC de ' +
-      'Língua Portuguesa é garantir a todos os alunos o acesso aos saberes ' +
-      'linguísticos necessários para a participação social e o exercício da ' +
-      'cidadania, pois é por meio da língua que o ser ' +
-      'humano pensa, comunica-se, tem acesso à informação, expressa e ' +
-      'defende pontos de vista, partilha ou constrói visões de mundo e ' +
-      'produz conhecimento.'),
-    new Disciplina(2, 'Educação Física', 'secundaria','2017-01-01',true,1, 'A Educação Física é o componente ' +
-      'curricular que tematiza as práticas corporais em suas diversas formas ' +
-      'de codificação e significação social, entendidas como manifestações ' +
-      'das possibilidades expressivas dos sujeitos e patrimônio cultural ' +
-      'da humanidade. Nessa concepção, o movimento humano está sempre ' +
-      'inserido no âmbito da cultura e não se limita a ' +
-      'um deslocamento espaço-temporal de um segmento corporal ' +
-      'ou de um corpo todo. Logo, as práticas corporais são textos culturais ' +
-      'passíveis de leitura e produção.'),
-    new Disciplina(3, 'Inglês', 'primaria','2017-01-01',true,1, 'Aprender a língua inglesa propicia a criação ' +
-      'de novas formas de engajamento e participação dos alunos em um mundo ' +
-      'social cada vez mais globalizado e plural, em que as fronteiras ' +
-      'entre países e interesses pessoais, locais, regionais, nacionais ' +
-      'e transnacionais estão cada vez mais difusas e contraditórias. ' +
-      'Assim, o estudo da língua inglesa possibilita aos alunos ampliar ' +
-      'horizontes de comunicação e de intercâmbio cultural, científico ' +
-      'e acadêmico e, nesse sentido, abre novos percursos de acesso, ' +
-      'construção de conhecimentos e participação social. É esse caráter ' +
-      'formativo que inscreve a aprendizagem de inglês em uma perspectiva ' +
-      'de educação linguística, consciente e crítica, na qual as dimensões ' +
-      'pedagógicas e políticas são intrinsecamente ligadas.'),
-    new Disciplina(4, 'Matemática','primaria','2017-01-01',true,1, 'No Ensino Fundamental, essa área, por ' +
-      'meio da articulação de seus diversos campos – Aritmética, Álgebra, ' +
-      'Geometria, Estatística e Probabilidade – precisa garantir que os ' +
-      'alunos relacionem observações empíricas do mundo real a ' +
-      'representações (tabelas, figuras e esquemas) e associem essas ' +
-      'representações a uma atividade matemática, conceitos e ' +
-      'propriedades, fazendo induções e conjecturas. Assim, espera-se ' +
-      'que eles desenvolvam a capacidade de identificar oportunidades ' +
-      'de utilização da matemática para resolver problemas, aplicando ' +
-      'conceitos, procedimentos e resultados para obter soluções e ' +
-      'interpretá-las segundo os contextos das situações. A dedução de ' +
-      'algumas propriedades e a verificação de conjecturas, a partir ' +
-      'de outras, podem ser estimuladas, sobretudo ao final do ' +
-      'Ensino Fundamental.'),
-    new Disciplina(5, 'Ciências','primaria','2017-01-01',true,1, 'Ao estudar Ciências, as pessoas aprendem ' +
-      'a respeito de si mesmas, da diversidade e dos processos de evolução ' +
-      'e manutenção da vida, do mundo material – com os seus recursos ' +
-      'naturais, suas transformações e fontes de energia –, do nosso ' +
-      'planeta no Sistema Solar e no Universo e da aplicação dos ' +
-      'conhecimentos científicos nas várias esferas da vida humana. ' +
-      'Essas aprendizagens, entre outras, possibilitam que os alunos ' +
-      'compreendam, expliquem e intervenham no mundo em que vivem.')
-  ];
+  disciplinas = this.getDisciplinas();
 
   salvar() {
     if (this.editando) {
@@ -80,17 +29,13 @@ export class AppComponent {
       this.editando.descricao = this.descricao;
       this.cancelar();
     } else {
+      let disciplinas = this.getDisciplinas();
       const d = new Disciplina(this.cod, this.nome, this.tipo,this.data, 
                               this.status,this.periodo, this.descricao);
       this.disciplinas.push(d);
+      this.setLocal(this.disciplinas);
     }
-    this.cod = null;
-    this.nome = null;
-    this.tipo = null;
-    this.data = null; 
-    this.status = null;
-    this.periodo = null;
-    this.descricao = null;
+    this.cancelar();
   }
 
   excluir(disciplina) {
@@ -99,8 +44,11 @@ export class AppComponent {
     } else {
       if (confirm('Tem certeza que deseja excluir a disciplina "'
           + disciplina.nome + '"?')) {
+        let disciplinas = this.getDisciplinas();
         const i = this.disciplinas.indexOf(disciplina);
-        this.disciplinas.splice(i, 1);
+        disciplinas.splice(i, 1);
+        this.disciplinas = disciplinas
+        this.setLocal(disciplinas);
       }
     }
   }
@@ -111,7 +59,7 @@ export class AppComponent {
     this.tipo = disciplina.tipo;
     this.data = disciplina.data;
     this.status = disciplina.status;
-    this.periodo = disciplina.periodo;
+    this.periodo = this.periodos.indexOf(disciplina.periodo);
     this.descricao = disciplina.descricao;
     this.editando = disciplina;
   }
@@ -125,18 +73,19 @@ export class AppComponent {
     this.periodo = null;
     this.descricao = null;
   }
-  insert() {
-    if(this.incluir) {
-      alert('Insira os dados no formulários e salve');
+
+  getDisciplinas(){
+    let localStorageItem = JSON.parse(localStorage.getItem('disciplinas'))
+    if (localStorageItem == null) {
+      return []
+    } else {
+      return localStorageItem.disciplinas;
     }
-    else {
-      this.cod = null;
-      this.nome = null;
-      this.tipo = null;
-      this.data = null; 
-      this.status = null;
-      this.periodo = null;
-      this.descricao = null;
-    }
+    
   }
+
+  setLocal(disciplinas) {
+    localStorage.setItem('disciplinas', JSON.stringify({disciplinas}));
+  }
+
 }
