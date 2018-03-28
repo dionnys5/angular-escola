0168be8d8536b546fea496e055bf05a448c6952d
DIONNYS SANTOS MARINHO <121095910@labin.canoas>
2018-03-27 21:43:48 -0300

    Formulario angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,29 +9,37 @@ import {Disciplina} from './disciplina.model';
 export class AppComponent {
   incluir = null;
   editando = null;
-  cod = null;
+  cod = -1;
   nome = null;
-  tipo = null;
+  tipo = 'primaria';
   data = null;
   status = false;
-  periodos = [1,2,3,4,5,6,7,8]
-  periodo = null;
+  periodos = [1, 2, 3, 4, 5, 6, 7, 8];
+  periodo = '';
   descricao = null;
   disciplinas = this.getDisciplinas();
 
   salvar() {
     if (this.editando) {
-      this.editando.cod = this.cod;
-      this.editando.nome = this.nome;
-      this.editando.tipo = this.tipo;
-      this.editando.data = this.data;
-      this.editando.status = this.status;
-      this.editando.descricao = this.descricao;
+      const edit = this.getDisciplinas();
+      const i = edit.indexOf(this.editando);
+      console.log(i);
+      /*
+            edit[i].cod = this.cod;
+      edit[i].nome = this.nome;
+      edit[i].tipo = this.tipo;
+      edit[i].data = this.data;
+      edit[i].status = this.status;
+      edit[i].descricao = this.descricao;
+      edit[i].periodo = this.periodo;
+       */
+
+      this.setLocal(edit);
       this.cancelar();
     } else {
-      let disciplinas = this.getDisciplinas();
-      const d = new Disciplina(this.cod, this.nome, this.tipo,this.data, 
-                              this.status,this.periodo, this.descricao);
+        const disciplinas = this.getDisciplinas();
+      const d = new Disciplina(this.cod, this.nome, this.tipo, this.data,
+                              this.status, this.periodo, this.descricao);
       this.disciplinas.push(d);
       this.setLocal(this.disciplinas);
     }
@@ -39,15 +47,15 @@ export class AppComponent {
   }
 
   excluir(disciplina) {
-    if (this.editando == disciplina) {
+    if (this.editando === disciplina) {
       alert('Você não pode excluir uma disciplina que está editando');
     } else {
       if (confirm('Tem certeza que deseja excluir a disciplina "'
           + disciplina.nome + '"?')) {
-        let disciplinas = this.getDisciplinas();
+        const disciplinas = this.getDisciplinas();
         const i = this.disciplinas.indexOf(disciplina);
         disciplinas.splice(i, 1);
-        this.disciplinas = disciplinas
+        this.disciplinas = disciplinas;
         this.setLocal(disciplinas);
       }
     }
@@ -59,29 +67,30 @@ export class AppComponent {
     this.tipo = disciplina.tipo;
     this.data = disciplina.data;
     this.status = disciplina.status;
-    this.periodo = this.periodos.indexOf(disciplina.periodo);
+    this.periodo = disciplina.periodo;
     this.descricao = disciplina.descricao;
     this.editando = disciplina;
+    console.log(this.editando);
   }
 
   cancelar() {
     this.cod = null;
     this.nome = null;
     this.tipo = null;
-    this.data = null; 
+    this.data = null;
     this.status = null;
-    this.periodo = this.periodos[0];
+    this.periodo = '';
     this.descricao = null;
   }
 
-  getDisciplinas(){
-    let localStorageItem = JSON.parse(localStorage.getItem('disciplinas'))
+  getDisciplinas() {
+    const localStorageItem = JSON.parse(localStorage.getItem('disciplinas'));
     if (localStorageItem == null) {
-      return []
+      return [];
     } else {
       return localStorageItem.disciplinas;
     }
-    
+
   }
 
   setLocal(disciplinas) {
